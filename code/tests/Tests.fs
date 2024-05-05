72cf4a7f7ab64c86a276f9304cde931c68d29f9b
harryalbert <harryalbert364@gmail.com>
2024-05-05 15:21:33 -0400

    check

---
+++ code/tests/Tests.fs
@@ -0,0 +1,18 @@
+namespace tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Parser
+
+[<TestClass>]
+type TestClass() =
+
+    [<TestMethod>]
+    member this.TestMethodPassing() =
+        let res = parse "hello world"
+
+        match res with
+        | Some(ast) ->
+            let expected = Lines([ FormattedTexts([ String "hello world" ]) ])
+            Assert.AreEqual(expected, ast)
+        | None -> Assert.IsTrue(false)
