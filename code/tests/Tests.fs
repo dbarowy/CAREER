d6322fdfb498d1a45f4b5d8a4703d0f375e43e71
harryalbert <harryalbert364@gmail.com>
2024-05-13 22:15:37 -0400

    check

--- code/tests/Tests.fs
+++ code/tests/Tests.fs
@@ -1,5 +1,6 @@
 namespace tests
 
+open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open AST
 open Parser
@@ -10,9 +11,17 @@ type TestClass() =
 
     [<TestMethod>]
     member this.TestStringParsing() =
-        let res = parse "hello world"
+        // lots of directory work to find the location of the test input
+        let c_dir = System.Environment.CurrentDirectory
+        let split_dir = c_dir.Split [| '/' |]
+        let test_dir_index = Array.findIndex (fun dir -> dir = "tests") split_dir
+        let split_test_dir = Array.take (test_dir_index + 1) split_dir
+        let test_dir = (String.concat "/" split_test_dir) + "/test_input.txt"
 
-        match res with
+        let lines = System.IO.File.ReadAllText test_dir
+        let maybe_ast = parse lines
+
+        match maybe_ast with
         | Some(ast) ->
             let expected = Lines([ FormattedTexts([ String "hello world" ]) ])
             Assert.AreEqual(expected, ast)
@@ -20,12 +29,28 @@ type TestClass() =
 
     [<TestMethod>]
     member this.TestStringInterpretation() =
-        let maybe_ast = parse "hello world"
+        // lots of directory work to find the location of the test input
+        let c_dir = System.Environment.CurrentDirectory
+        let split_dir = c_dir.Split [| '/' |]
+        let test_dir_index = Array.findIndex (fun dir -> dir = "tests") split_dir
+        let split_test_dir = Array.take (test_dir_index + 1) split_dir
+        let test_dir = (String.concat "/" split_test_dir) + "/test_input.txt"
+
+        let lines = System.IO.File.ReadAllText test_dir
+        let maybe_ast = parse lines
 
         match maybe_ast with
         | Some(ast) ->
             match ast with
             | Lines(lines) ->
+                (*
+                    this is the closest to end to end testing I can get without directly checking a pdf or latex code.
+                    checking latex code against my own latex code feels a bit silly, as the test will have to change 
+                    any time I add any new preamble or feature to the latex. Checkign the raw string output feels 
+                    closer to a true check of functionality.
+                *)
+
+
                 let list_caps = find_list_caps ast
                 let res = (evalLines lines list_caps)
                 let expected = "hello world\\\\ \n"
